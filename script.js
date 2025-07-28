1ea9ef9ddd0652f2222c120d7f97067b2a707f23
OneIs2much <3299034354@qq.com>
2025-07-28 19:02:26 +0800

    搭个框架先

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("new-game").addEventListener("click", () => {
+    alert("开始新游戏功能待实现");
+});
+
+document.getElementById("load-game").addEventListener("click", () => {
+    alert("读取上次游玩进度功能待实现");
+});
+
+document.getElementById("settings").addEventListener("click", () => {
+    alert("游戏设置功能待实现");
+});
